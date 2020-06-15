c835ba197155f28ddcf9c9642a03e61814fa6dfb
phanikanthm <phani010203@gmail.com>
2020-06-15 19:35:36 +0530

    do2

---
+++ dom1.js
@@ -0,0 +1,34 @@
+// //ParentNode ParentElement
+
+ var itemlist = document.querySelector('#items');
+// console.log(itemlist.parentNode);
+// itemlist.parentNode.style.backgroundColor = 'lightgrey';
+
+// var itemlist = document.querySelector('#items');
+// console.log(itemlist.parentElement);
+// itemlist.parentElement.style.backgroundColor = 'lightgrey';
+// console.log(itemlist.parentElement.parentElement.parentElement);
+
+// //Childnodes
+// console.log(itemlist.children);
+// console.log(itemlist.children[1]);
+// itemlist.children[1].style.backgroundColor='yellow';
+// itemlist.children[1].textContent='hello2';
+
+
+// // firstchild
+// console.log(itemlist.firstChild);
+
+// // firstElementChild
+// console.log(itemlist.firstElementChild);
+// itemlist.firstElementChild.textContent='hello';
+
+
+// // lastchild
+// console.log(itemlist.lastChild);
+
+// // lastElementChild
+// console.log(itemlist.lastElementChild);
+// itemlist.lastElementChild.textContent='hello4';
+
+
